77e268364682fb76b342b47143326737735773c5
Arjuna Nayak <arjun881@gmail.com>
2025-06-21 18:42:54 +0530

    fifth

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ app.use('/api/contact', contactRoutes);
 app.use('/api/bookings', bookingRoutes);
 app.use('/api/newsletter', newsletterRoutes);
 
-const __dirname = path.resolve();
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')));
-  app.get('*', (req, res) =>
-    res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
-  );
-}
+// const __dirname = path.resolve();
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '/frontend/build')));
+//   app.get('*', (req, res) =>
+//     res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+//   );
+// }
 
 const PORT = process.env.PORT || 5000;
 
